b01a9a81ec885742d07c1964ea952e9be913e9fe
BP <valkarian@valkarian.com>
2025-04-01 14:04:57 +0200

    Initial commit

---
+++ src/test/java/org/example/jee_lab/JeeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jee_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
