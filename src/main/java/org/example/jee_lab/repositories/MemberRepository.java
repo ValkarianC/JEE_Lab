dd1e3256f240993e4df240cb8304a68edc28c86f
BP <valkarian@valkarian.com>
2025-04-02 11:06:27 +0200

    Task: MemberRepository now extends JpaRepository

--- src/main/java/org/example/jee_lab/repositories/MemberRepository.java
+++ src/main/java/org/example/jee_lab/repositories/MemberRepository.java
@@ -1,4 +1,9 @@
 package org.example.jee_lab.repositories;
 
-public interface MemberRepository {
+import org.example.jee_lab.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
