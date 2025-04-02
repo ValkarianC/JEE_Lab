4186ccecad8a4389c0ef31e8ca9967c0135ff973
BP <valkarian@valkarian.com>
2025-04-02 11:05:50 +0200

    Task: Added attributes/constructors/getters/setters to Member and Address class

--- src/main/java/org/example/jee_lab/entities/Address.java
+++ src/main/java/org/example/jee_lab/entities/Address.java
@@ -1,4 +1,61 @@
 package org.example.jee_lab.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+
+    @Column(length = 40, nullable = false)
+    private String street;
+
+    @Column(length = 7, nullable = false)
+    private String postalCode;
+
+    @Column(length = 20, nullable = false)
+    private String city;
+
+    public Address() {
+    }
+
+    public Address(long ID, String street, String postalCode, String city) {
+        this.ID = ID;
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- src/main/java/org/example/jee_lab/entities/Member.java
+++ src/main/java/org/example/jee_lab/entities/Member.java
@@ -1,4 +1,98 @@
 package org.example.jee_lab.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+    @Column(length = 30, nullable = false)
+    private String firstName;
+    @Column(length = 30, nullable = false)
+    private String lastName;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinTable(name = "member_address",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private Address address;
+
+    @Column(length = 60, nullable = false)
+    private String email;
+    @Column(length = 15, nullable = true)
+    private String phone;
+    @Column(length = 10, nullable = false)
+    private LocalDate date;
+
+    public Member() {
+    }
+
+    public Member(long ID, String firstName, String lastName, Address address, String email, String phone, LocalDate date) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.date = date;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
